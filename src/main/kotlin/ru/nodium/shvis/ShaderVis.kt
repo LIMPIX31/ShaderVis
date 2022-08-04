fb9f611c5497ace4e19e161cebd2b8ad0389d69c
LIMPIX31 <limpix31@gmail.com>
2022-08-04 23:24:10 +0200

    init refactoring

---
+++ src/main/kotlin/ru/nodium/shvis/ShaderVis.kt
@@ -0,0 +1,13 @@
+package ru.nodium.shvis
+
+import net.fabricmc.api.ModInitializer
+
+class ShaderVis : ModInitializer {
+  companion object {
+    val LOGGER: org.slf4j.Logger = org.slf4j.LoggerFactory.getLogger("shvis")
+  }
+
+  override fun onInitialize() {
+
+  }
+}
